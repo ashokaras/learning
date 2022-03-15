0c7b17d1c81a749e01c0e6eec0e0d7259856b5e8
Ashok Arasu <ashoklord@gmail.com>
2022-03-15 13:10:10 +0530

    OneDotOne.java file is updated

--- src/sampleOne/OneDotOne.java
+++ src/sampleOne/OneDotOne.java
@@ -3,7 +3,10 @@ package sampleOne;
 public class OneDotOne {
 
     public static void main(String[] args) {
+
         System.out.println("One.One");
+        System.out.println("Sample updated");
+
     }
 
 }
