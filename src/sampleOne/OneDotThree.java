b66e501936bd6e5fa58bb65bd29d11df1ed3a92e
ashokaras <41256946+ashokaras@users.noreply.github.com>
2022-03-15 13:11:22 +0530

    Update OneDotThree.java
    Update from Git hub to check pulling of updates

--- src/sampleOne/OneDotThree.java
+++ src/sampleOne/OneDotThree.java
@@ -4,6 +4,7 @@ public class OneDotThree {
 
     public static void main(String[] args) {
         System.out.println("One.Three");
+        System.out.println("Update from Git hub to check pulling of updates");
     }
 
 }
